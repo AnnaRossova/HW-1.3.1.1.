77fe2a8de4d2e756cbca445a880d463bff8a2d6d
AnnaRossova <annarossova@gmail.com>
2020-02-24 20:35:39 +0600

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double ticketPrice = 6797.50;
+        int milePrice = 20;
+        int totalMile = (int) ticketPrice / milePrice;
+        System.out.println(totalMile);
+    }
+
+}
